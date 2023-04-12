1a477e926368c773f362328c531734baf524b4c4
Luis Tejada <luetejava@gmail.com>
2023-04-11 21:18:53 -0500

    fin

--- components/ui/EntryCard.tsx
+++ components/ui/EntryCard.tsx
@@ -1,8 +1,11 @@
 import { DragEvent, FC, useContext } from 'react';
+import { useRouter } from 'next/router';
 import { Card, CardActionArea, CardActions, CardContent, Typography } from '@mui/material';
 
 import { UIContext } from '../../context/ui/UIContext';
 import { Entry } from '../../interfaces';
+
+import { dateFunctions } from '../../utils';
 
 
 interface Props {
@@ -12,6 +15,7 @@ interface Props {
 export const EntryCard:FC<Props>= ({ entry }) => {
 
     const { startDragging, endDragging } = useContext( UIContext );
+    const router = useRouter()
 
     const onDragStart = ( event: DragEvent ) => {
         event.dataTransfer.setData('text', entry._id );
@@ -23,9 +27,14 @@ export const EntryCard:FC<Props>= ({ entry }) => {
         endDragging();
     }
 
+    const onClick = () => {
+        router.push(`/entries/${ entry._id }`);
+    }
+
 
   return (
     <Card
+        onClick={ onClick }
         sx={{ marginBottom: 1 }}
         // Eventos de drag
         draggable
@@ -38,7 +47,7 @@ export const EntryCard:FC<Props>= ({ entry }) => {
             </CardContent>
 
             <CardActions sx={{ display: 'flex', justifyContent: 'end', paddingRight: 2 }}>
-                <Typography variant='body2'>hace 30 minutos</Typography>
+                <Typography variant='body2'>{ dateFunctions.getFormatDistanceToNow( entry.createdAt ) }</Typography>
             </CardActions>
         </CardActionArea>
     </Card>
